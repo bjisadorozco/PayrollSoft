d1c0a89ac3f7bb5cf211e4c2356395540f68ac5e
brayan orozco <yisad_orozco@outlook.com>
2021-10-15 20:01:10 -0500

    Agregar archivos de proyecto.

---
+++ PayrollGUI/FormPrincipal.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace PayrollGUI
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.pnlApartados = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEmpresa = new System.Windows.Forms.Button();
+            this.pnlEsquina3 = new System.Windows.Forms.Panel();
+            this.pnlEsquina = new System.Windows.Forms.Panel();
+            this.btnPersonas = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnConfiguracion = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnNomina = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pnlVista = new System.Windows.Forms.Panel();
+            this.pnlApartados.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlApartados
+            // 
+            this.pnlApartados.BackColor = System.Drawing.Color.DarkOrange;
+            this.pnlApartados.Controls.Add(this.panel4);
+            this.pnlApartados.Controls.Add(this.btnNomina);
+            this.pnlApartados.Controls.Add(this.panel3);
+            this.pnlApartados.Controls.Add(this.btnConfiguracion);
+            this.pnlApartados.Controls.Add(this.panel2);
+            this.pnlApartados.Controls.Add(this.btnReportes);
+            this.pnlApartados.Controls.Add(this.panel1);
+            this.pnlApartados.Controls.Add(this.btnEmpresa);
+            this.pnlApartados.Controls.Add(this.pnlEsquina3);
+            this.pnlApartados.Controls.Add(this.pnlEsquina);
+            this.pnlApartados.Controls.Add(this.btnPersonas);
+            this.pnlApartados.Controls.Add(this.btnInicio);
+            this.pnlApartados.Controls.Add(this.pictureLogo);
+            this.pnlApartados.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlApartados.Location = new System.Drawing.Point(0, 0);
+            this.pnlApartados.Name = "pnlApartados";
+            this.pnlApartados.Size = new System.Drawing.Size(164, 510);
+            this.pnlApartados.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Location = new System.Drawing.Point(0, 319);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(6, 43);
+            this.panel1.TabIndex = 12;
+            // 
+            // btnEmpresa
+            // 
+            this.btnEmpresa.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnEmpresa.FlatAppearance.BorderSize = 0;
+            this.btnEmpresa.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnEmpresa.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnEmpresa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpresa.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEmpresa.Location = new System.Drawing.Point(3, 319);
+            this.btnEmpresa.Name = "btnEmpresa";
+            this.btnEmpresa.Size = new System.Drawing.Size(161, 43);
+            this.btnEmpresa.TabIndex = 11;
+            this.btnEmpresa.Text = "EMPRESA";
+            this.btnEmpresa.UseVisualStyleBackColor = false;
+            this.btnEmpresa.Click += new System.EventHandler(this.btnEmpresa_Click);
+            // 
+            // pnlEsquina3
+            // 
+            this.pnlEsquina3.BackColor = System.Drawing.Color.Indigo;
+            this.pnlEsquina3.Location = new System.Drawing.Point(0, 227);
+            this.pnlEsquina3.Name = "pnlEsquina3";
+            this.pnlEsquina3.Size = new System.Drawing.Size(6, 43);
+            this.pnlEsquina3.TabIndex = 2;
+            // 
+            // pnlEsquina
+            // 
+            this.pnlEsquina.BackColor = System.Drawing.Color.Indigo;
+            this.pnlEsquina.Location = new System.Drawing.Point(0, 135);
+            this.pnlEsquina.Name = "pnlEsquina";
+            this.pnlEsquina.Size = new System.Drawing.Size(6, 43);
+            this.pnlEsquina.TabIndex = 0;
+            // 
+            // btnPersonas
+            // 
+            this.btnPersonas.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnPersonas.FlatAppearance.BorderSize = 0;
+            this.btnPersonas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnPersonas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnPersonas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPersonas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPersonas.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnPersonas.Location = new System.Drawing.Point(3, 227);
+            this.btnPersonas.Name = "btnPersonas";
+            this.btnPersonas.Size = new System.Drawing.Size(161, 43);
+            this.btnPersonas.TabIndex = 3;
+            this.btnPersonas.Text = "PERSONAS";
+            this.btnPersonas.UseVisualStyleBackColor = false;
+            this.btnPersonas.Click += new System.EventHandler(this.btnPersonas_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnInicio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnInicio.Location = new System.Drawing.Point(3, 135);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnInicio.Size = new System.Drawing.Size(161, 43);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "INICIO";
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.BackColor = System.Drawing.Color.DarkOrange;
+            this.pictureLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureLogo.Image")));
+            this.pictureLogo.Location = new System.Drawing.Point(0, 0);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(164, 135);
+            this.pictureLogo.TabIndex = 0;
+            this.pictureLogo.TabStop = false;
+            this.pictureLogo.Click += new System.EventHandler(this.pictureLogo_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnReportes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnReportes.Location = new System.Drawing.Point(3, 273);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(161, 43);
+            this.btnReportes.TabIndex = 13;
+            this.btnReportes.Text = "REPORTES";
+            this.btnReportes.UseVisualStyleBackColor = false;
+            this.btnReportes.Click += new System.EventHandler(this.btnReportes_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Location = new System.Drawing.Point(0, 273);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(6, 43);
+            this.panel2.TabIndex = 10;
+            // 
+            // btnConfiguracion
+            // 
+            this.btnConfiguracion.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnConfiguracion.FlatAppearance.BorderSize = 0;
+            this.btnConfiguracion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnConfiguracion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnConfiguracion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfiguracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfiguracion.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnConfiguracion.Location = new System.Drawing.Point(3, 366);
+            this.btnConfiguracion.Name = "btnConfiguracion";
+            this.btnConfiguracion.Size = new System.Drawing.Size(161, 43);
+            this.btnConfiguracion.TabIndex = 14;
+            this.btnConfiguracion.Text = "CONFIGURACION";
+            this.btnConfiguracion.UseVisualStyleBackColor = false;
+            this.btnConfiguracion.Click += new System.EventHandler(this.btnConfiguracion_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Indigo;
+            this.panel3.Location = new System.Drawing.Point(0, 366);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(6, 43);
+            this.panel3.TabIndex = 10;
+            // 
+            // btnNomina
+            // 
+            this.btnNomina.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnNomina.FlatAppearance.BorderSize = 0;
+            this.btnNomina.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Indigo;
+            this.btnNomina.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Indigo;
+            this.btnNomina.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNomina.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNomina.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnNomina.Location = new System.Drawing.Point(3, 181);
+            this.btnNomina.Name = "btnNomina";
+            this.btnNomina.Size = new System.Drawing.Size(161, 43);
+            this.btnNomina.TabIndex = 15;
+            this.btnNomina.Text = "NOMINA";
+            this.btnNomina.UseVisualStyleBackColor = false;
+            this.btnNomina.Click += new System.EventHandler(this.btnNomina_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Indigo;
+            this.panel4.Location = new System.Drawing.Point(0, 181);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(6, 43);
+            this.panel4.TabIndex = 2;
+            // 
+            // pnlVista
+            // 
+            this.pnlVista.BackColor = System.Drawing.SystemColors.Control;
+            this.pnlVista.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlVista.Location = new System.Drawing.Point(164, 0);
+            this.pnlVista.Name = "pnlVista";
+            this.pnlVista.Size = new System.Drawing.Size(674, 510);
+            this.pnlVista.TabIndex = 1;
+            this.pnlVista.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlVista_Paint);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 510);
+            this.Controls.Add(this.pnlVista);
+            this.Controls.Add(this.pnlApartados);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.pnlApartados.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlApartados;
+        private System.Windows.Forms.Button btnPersonas;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.Windows.Forms.Panel pnlEsquina3;
+        private System.Windows.Forms.Panel pnlEsquina;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEmpresa;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnConfiguracion;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnNomina;
+        private System.Windows.Forms.Panel pnlVista;
+    }
+}
---
+++ PayrollGUI/FormPrincipal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayrollGUI
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(new FormInicio());
+        }
+
+        private void abrirFormulario(object panelControl)
+        {
+            if (this.pnlVista.Controls.Count > 0)
+                this.pnlVista.Controls.RemoveAt(0);
+            Form formulario = panelControl as Form;
+            formulario.TopLevel = false;
+            formulario.Dock = DockStyle.Fill;
+            this.pnlVista.Controls.Add(formulario);
+            this.pnlVista.Tag = formulario;
+            formulario.Show();
+        }
+
+        private void btnPersonas_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(new FormPersonas());
+        }
+
+        private void pictureLogo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEmpresa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReportes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfiguracion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNomina_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlVista_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
